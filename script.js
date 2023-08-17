ab87552d495251a13cdda1f6a32a827d197fe99e
neeraj <neerajkumarghildiyal@gmail.com>
2023-08-17 21:27:05 +0530

    completed lecture38 also removed the repetition of code and followed the Dry principle

--- script.js
+++ script.js
@@ -1,8 +1,65 @@
-// Unplash API 
+const imageContainer = document.getElementById('image-container')
+const loader = document.getElementById('loader')
 
+let photosArray = [];
+
+// Unplash API 
+// Unplash username : Neer84472
 const count = 10;
 const apiKey ='4bdZ5ID8aZU0Yp8zoqcD99ruwcGYbFWspq04oKKV3Rg'; 
 const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`
+
+// Need to create a helper function coz we need to follow DRY(dont repeat yourself) principle 
+
+function setAttributes(element, attributes){
+
+  for(const key in attributes){
+
+    element.setAttribute(key, attributes[key])
+  }
+
+}
+
+// Create Elements for Links and Photos, Add to DOM
+
+function displayPhotos(){
+
+  //  Run function for FOR each object in PhotosArray
+
+  photosArray.forEach((photo) => {
+  
+   // Create <a> to link to Unsplash 
+   
+   const item = document.createElement('a');
+  //  item.setAttribute('href', photo.links.html) 
+  //  item.setAttribute('target', '_blank')
+  setAttributes(item, {
+
+    href: photo.links.html,
+    target: '_blank'
+  }) 
+  //  Create Image for photo 
+  const img = document.createElement('img')
+  // img.setAttribute('src', photo.urls.regular)
+  // img.setAttribute('alt', photo.alt_description)
+  // img.setAttribute('title', photo.alt_description)
+
+  setAttributes(img, {
+
+    src: photo.urls.regular,
+    alt: photo.alt_description,
+    title: photo.alt_description,
+  })
+
+  // Put <img> inside <a> element, then put both inside image container element
+ 
+  item.appendChild(img);
+  imageContainer.appendChild(item); 
+
+ 
+  });
+
+}
 
 // Get Photos from unsplash API 
 
@@ -11,8 +68,11 @@ async function getPhotos(){
     try {
 
     const response = await fetch(apiURL);
-    const data = await response.json();
-    console.log(data); 
+    photosArray = await response.json();
+    // console.log(data); 
+    // console.log(photosArray)
+
+    displayPhotos(); 
 
     } catch (error) {
       // Catch Error Here  
